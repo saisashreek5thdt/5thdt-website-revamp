9c3a5e7c1d3c4899b21c517ef113113543d9427e
Sai Sashreek D <saisashreekd@gmail.com>
2025-03-03 11:45:33 +0530

    Folder and File Structure Updated

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -2,9 +2,9 @@ const themeToggle = document.getElementById("theme-toggle");
 const themeStyle = document.getElementById("theme-style");
 
 themeToggle.addEventListener("click", () => {
-  if (themeStyle.getAttribute("href") === "css/light.min.css") {
-    themeStyle.setAttribute("href", "css/dark.min.css");
+  if (themeStyle.getAttribute("href") === "assets/content/css/light.min.css") {
+    themeStyle.setAttribute("href", "assets/content/css/dark.min.css");
   } else {
-    themeStyle.setAttribute("href", "css/light.min.css");
+    themeStyle.setAttribute("href", "assets/content/css/light.min.css");
   }
 });
